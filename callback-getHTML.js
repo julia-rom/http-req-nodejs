dcd9d849ea3c6f4a3fc3bc507d720bfb9ca8dee6
Julia <juliaromanowski@gmail.com>
2019-01-14 18:11:25 +0000

    added callback as parameter

---
+++ callback-getHTML.js
@@ -0,0 +1,33 @@
+var https = require('https');
+
+function getHTML(options, callback) {
+
+    https.get(options, function (response, body) {
+
+        response.setEncoding('utf8');
+
+
+        response.on('data', function (data) {
+            var dataText = [];
+            dataText.push(data);
+            printHTML(data);
+        });
+
+        response.on('end', function () {
+            console.log('Response stream complete.');
+        });
+
+
+    });
+
+}
+
+function printHTML(html) {
+    console.log(html);
+}
+
+
+getHTML(requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+})
